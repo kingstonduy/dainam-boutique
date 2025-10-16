80569a985b30f42e859ffead82d95e39e68133dd
kingstonduy <kingstonduy.duong@gmail.com>
2025-10-16 22:53:12 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+    return (
+        <>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+                <h1 className="text-4xl font-bold text-blue-600 mb-4">Hello, Vite + React + Tailwind CSS!</h1>
+                <p className="text-lg text-gray-700">This is a simple setup to get you started.</p>
+                <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 hover:cursor-pointer transition">Click Me</button>
+            </div>
+        </>
+    );
+}
+
+export default App;
