3339ef29683ec4894dd80fd72831a515e03a06bc
duydk3 <duydk3@vng.com.vn>
2025-11-20 00:23:09 +0700

    feature/handle both github and cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-            "@/components": path.resolve(__dirname, "./src/components"),
-            "@/lib": path.resolve(__dirname, "./src/lib"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        base: env.VITE_BASENAME || "/",
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+                "@/components": path.resolve(__dirname, "./src/components"),
+                "@/lib": path.resolve(__dirname, "./src/lib"),
+            },
         },
-    },
+    };
 });
